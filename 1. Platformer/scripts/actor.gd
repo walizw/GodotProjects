e881b4b600574bcf33e96625f41879ba34a582b9
Sara <yojan.bustamante@udea.edu.co>
2022-11-01 12:44:36 -0500

    limiting the player speed

--- 1. Platformer/scripts/actor.gd
+++ 1. Platformer/scripts/actor.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 class_name Actor
 
+export var speed: = Vector2 (300.0, 1000.0)
 export var grav: = 3000.0
 var velocity: = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	velocity.y += grav * delta
-	move_and_slide (velocity)
+	velocity.y = min (speed.y, velocity.y)
+	velocity = move_and_slide (velocity)
