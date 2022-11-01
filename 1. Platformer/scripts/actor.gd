4af670c8e663a56481b50ad9ef97cd1a2697b9ff
Sara <yojan.bustamante@udea.edu.co>
2022-11-01 12:36:14 -0500

    starting to code character movement

--- 1. Platformer/scripts/actor.gd
+++ 1. Platformer/scripts/actor.gd
@@ -1,2 +1,7 @@
 extends KinematicBody2D
 class_name Actor
+
+func _physics_process(delta: float) -> void:
+	# executed every frame (30/60 times a sec)
+	var velocity: = Vector2 (300, 0)
+	move_and_slide (velocity)
