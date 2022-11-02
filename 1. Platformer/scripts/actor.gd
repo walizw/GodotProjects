67dac0135230acc4c147d06a361444bdd3b2498d
Sara <yojan.bustamante@udea.edu.co>
2022-11-02 07:37:35 -0500

    player jump and interrupting jump

--- 1. Platformer/scripts/actor.gd
+++ 1. Platformer/scripts/actor.gd
@@ -1,10 +1,8 @@
 extends KinematicBody2D
 class_name Actor
 
+const FLOOR_NORMAL: = Vector2.UP
+
 export var speed: = Vector2 (300.0, 1000.0)
 export var grav: = 3000.0
 var velocity: = Vector2.ZERO
-
-func _physics_process(delta: float) -> void:
-	velocity.y += grav * delta
-	velocity.y = min (speed.y, velocity.y)
--- 1. Platformer/scripts/player.gd
+++ 1. Platformer/scripts/player.gd
@@ -1,12 +1,29 @@
 extends Actor
 
 func _physics_process (delta: float) -> void:
+	var is_jump_interrupted := Input.is_action_just_released ("jump") and velocity.y < 0.0
 	var dir: = get_direction ()
-	velocity = dir * speed
-	velocity = move_and_slide (velocity)
+	velocity = calculate_mov_velocity (velocity, speed, dir, is_jump_interrupted)
+	velocity = move_and_slide (velocity, FLOOR_NORMAL)
 
 func get_direction () -> Vector2:
 	return Vector2 (
 		Input.get_action_strength ("right") - Input.get_action_strength ("left"),
 		-1.0 if Input.is_action_just_pressed ("jump") and is_on_floor () else 1.0
 	)
+
+func calculate_mov_velocity (
+		linear_velocity: Vector2,
+		speed: Vector2,
+		dir: Vector2,
+		jump_interrupted: bool
+	) -> Vector2:
+	var new_velocity = linear_velocity
+	new_velocity.x = speed.x * dir.x
+	new_velocity.y += grav * get_physics_process_delta_time ()
+	if dir.y == -1.0:
+		new_velocity.y = speed.y * dir.y
+	
+	if jump_interrupted:
+		new_velocity.y = 0.0
+	return new_velocity
