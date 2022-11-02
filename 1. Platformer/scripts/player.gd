61702eab8a23dbaef45ed8519106b7e965ae9b4f
Sara <yojan.bustamante@udea.edu.co>
2022-11-02 07:20:03 -0500

    calculating the jump input

--- 1. Platformer/scripts/player.gd
+++ 1. Platformer/scripts/player.gd
@@ -1,10 +1,12 @@
 extends Actor
 
 func _physics_process (delta: float) -> void:
-	var dir: = Vector2 (
-		Input.get_action_strength ("right") - Input.get_action_strength ("left"),
-		1.0
-	)
-	
+	var dir: = get_direction ()
 	velocity = dir * speed
 	velocity = move_and_slide (velocity)
+
+func get_direction () -> Vector2:
+	return Vector2 (
+		Input.get_action_strength ("right") - Input.get_action_strength ("left"),
+		-1.0 if Input.is_action_just_pressed ("jump") and is_on_floor () else 1.0
+	)
