e1015a762228e3d8d7c53153758767bd30fe7b57
Sara <yojan.bustamante@udea.edu.co>
2022-11-07 23:20:59 +0000

    creating the enemy

---
+++ 1. Platformer/scripts/enemy.gd
@@ -0,0 +1,13 @@
+extends Actor
+
+func _ready() -> void:
+	_velocity.x = -speed.x
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += grav * delta
+	
+	if is_on_wall ():
+		# change direction
+		_velocity.x *= -1.0
+	
+	_velocity.y = move_and_slide (_velocity, FLOOR_NORMAL).y
