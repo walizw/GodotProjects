fd2054f2c9aed0e0e9c29d97f000f40e8a1aa029
Sara <yojan.bustamante@udea.edu.co>
2022-11-08 16:27:05 +0000

    making the player die

--- 1. Platformer/scripts/enemy.gd
+++ 1. Platformer/scripts/enemy.gd
@@ -4,6 +4,13 @@ func _ready() -> void:
 	set_physics_process(false) # deactivate physics_process at the start
 	_velocity.x = -speed.x
 
+func _on_StompDetector_body_entered(body: Node) -> void:
+	if body.global_position.y > get_node ("StompDetector").global_position.y:
+		return
+	
+	get_node ("CollisionShape2D").disabled = true # disable the "CollisionShape2D"
+	queue_free ()
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += grav * delta
 	
