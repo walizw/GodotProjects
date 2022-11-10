11c6819fd821d0ee9451b82cf0f618147621cbac
Sara <yojan.bustamante@udea.edu.co>
2022-11-10 23:39:31 +0000

    coded the coin

--- 1. Platformer/scripts/enemy.gd
+++ 1. Platformer/scripts/enemy.gd
@@ -5,10 +5,10 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 
 func _on_StompDetector_body_entered(body: Node) -> void:
-	if body.global_position.y > get_node ("StompDetector").global_position.y:
+	if body.global_position.y > $StompDetector.global_position.y:
 		return
 	
-	get_node ("CollisionShape2D").disabled = true # disable the "CollisionShape2D"
+	$CollisionShape2D.disabled = true # disable the "CollisionShape2D"
 	queue_free ()
 
 func _physics_process(delta: float) -> void:
