48478fcbc1adda9926853ead027f2a4b48846c70
Sara <yojan.bustamante@udea.edu.co>
2022-11-11 17:27:15 +0000

    now there are two levels the player can be teleported to

--- 1. Platformer/scripts/portal.gd
+++ 1. Platformer/scripts/portal.gd
@@ -4,7 +4,10 @@ extends Area2D
 export var next_scene: PackedScene
 onready var anim_player: AnimationPlayer = $AnimationPlayer
 
-func _get_configuration_warning() -> String:
+func _on_body_entered (body: Node) -> void:
+	teleport ()
+
+func _get_configuration_warning () -> String:
 	return "The next scene property can't be empty" if not next_scene else ""
 
 func teleport () -> void:
