890283bb4228e487004a5dc6f7bda64a27654831
Sara <yojan.bustamante@udea.edu.co>
2022-11-12 17:11:35 +0000

    score and deaths

--- 1. Platformer/scripts/enemy.gd
+++ 1. Platformer/scripts/enemy.gd
@@ -1,5 +1,7 @@
 extends Actor
 
+export var score: = 150
+
 func _ready() -> void:
 	set_physics_process(false) # deactivate physics_process at the start
 	_velocity.x = -speed.x
@@ -8,8 +10,8 @@ func _on_StompDetector_body_entered(body: KinematicBody2D) -> void:
 	if body.global_position.y > $StompDetector.global_position.y:
 		return
 	
-	$CollisionShape2D.disabled = true # disable the "CollisionShape2D"
-	queue_free ()
+	# $CollisionShape2D.disabled = true # disable the "CollisionShape2D"
+	die ()
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += grav * delta
@@ -20,3 +22,7 @@ func _physics_process(delta: float) -> void:
 	
 	var snap: = Vector2.DOWN * 50
 	_velocity.y = move_and_slide_with_snap (_velocity, snap, FLOOR_NORMAL).y
+
+func die () -> void:
+	queue_free ()
+	PlayerData.score += score
