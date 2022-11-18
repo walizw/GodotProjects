f87c6495b4d4a967808cad2280bba465ea8070cc
Sara <yojan.bustamante@udea.edu.co>
2022-11-18 18:14:01 +0000

    i forgot to push and deleted the local folder, so here's the finished gdquest project

--- 1. Platformer/scripts/ui/PlayBtn.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-export(String, FILE) var to_scene_path: = ""
-
-func _on_button_up () -> void:
-	get_tree ().change_scene (to_scene_path)
-
-func _get_configuration_warning () -> String:
-	return "You need to provide a \"to_scene_path\"" if to_scene_path == "" else ""
